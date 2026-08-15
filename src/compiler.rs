537d8fbd01cccafd08bb737fee1b31cd3d20ed05
Bruno <brmcerqueira@gmail.com>
2026-08-15 10:29:17 -0300

    Start

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,50 +1,45 @@
 use anyhow::Result;
-use melior::{
-    Context,
-    dialect::{DialectRegistry, arith, func},
-    ir::{
-        Block, BlockLike, Location, Module as MlirModule, Region, RegionLike,
-        attribute::{IntegerAttribute, StringAttribute, TypeAttribute},
-        r#type::{FunctionType, IntegerType},
-    },
-    utility::register_all_dialects,
+use inkwell::{
+    IntPredicate,
+    builder::Builder,
+    context::Context,
+    module::Module as LlvmModule,
+    values::BasicValueEnum,
 };
 use swc_ecma_ast::{BinaryOp, Decl, Expr, FnDecl, Lit, Module, ModuleItem, Pat, Stmt};
 
 pub fn compile(module: &Module) -> Result<()> {
-    let context = Context::new();
-    let registry = DialectRegistry::new();
-    register_all_dialects(&registry);
-    context.append_dialect_registry(&registry);
-    context.load_all_available_dialects();
-
-    let location = Location::unknown(&context);
-    let mlir_module = MlirModule::new(location);
+    let context = Context::create();
+    let llvm_module = context.create_module("tsnc");
+    let builder = context.create_builder();
 
     for item in &module.body {
         if let ModuleItem::Stmt(Stmt::Decl(Decl::Fn(function))) = item {
-            compile_function(&context, &mlir_module, function)?;
+            compile_function(&context, &llvm_module, &builder, function)?;
         }
     }
 
-    println!("{}", mlir_module.as_operation());
+    println!("{}", llvm_module.print_to_string().to_string());
     Ok(())
 }
 
-fn compile_function<'c>(
-    context: &'c Context,
-    mlir_module: &MlirModule<'c>,
+fn compile_function<'ctx>(
+    context: &'ctx Context,
+    llvm_module: &LlvmModule<'ctx>,
+    builder: &Builder<'ctx>,
     function: &FnDecl,
 ) -> Result<()> {
-    let location = Location::unknown(context);
-    let i64_type = IntegerType::new(context, 64).into();
+    let i64_type = context.i64_type();
 
     let param_types: Vec<_> = function
         .function
         .params
         .iter()
-        .map(|_| (i64_type, location))
+        .map(|_| i64_type.into())
         .collect();
+
+    let fn_type = i64_type.fn_type(&param_types, false);
+    let fn_value = llvm_module.add_function(&function.ident.sym, fn_type, None);
 
     let param_names: Vec<(String, usize)> = function
         .function
@@ -60,74 +55,77 @@ fn compile_function<'c>(
         })
         .collect();
 
-    let block = Block::new(&param_types);
+    let entry_block = context.append_basic_block(fn_value, "entry");
+    builder.position_at_end(entry_block);
 
     if let Some(body) = &function.function.body {
         for stmt in &body.stmts {
             if let Stmt::Return(ret) = stmt {
                 if let Some(arg) = &ret.arg {
-                    let value = compile_expr(context, &block, arg, &param_names, location)?;
-                    block.append_operation(func::r#return(&[value], location));
+                    let value = compile_expr(context, builder, fn_value, arg, &param_names)?;
+                    builder.build_return(Some(&value))?;
                 } else {
-                    block.append_operation(func::r#return(&[], location));
+                    builder.build_return(None)?;
                 }
             }
         }
     }
 
-    let region = Region::new();
-    region.append_block(block);
-
-    let n_params = function.function.params.len();
-    let func_type = FunctionType::new(context, &vec![i64_type; n_params], &[i64_type]);
-
-    mlir_module.body().append_operation(func::func(
-        context,
-        StringAttribute::new(context, &function.ident.sym),
-        TypeAttribute::new(func_type.into()),
-        region,
-        &[],
-        location,
-    ));
-
     Ok(())
 }
 
-fn compile_expr<'c, 'a>(
-    context: &'c Context,
-    block: &'a Block<'c>,
+fn compile_expr<'ctx>(
+    context: &'ctx Context,
+    builder: &Builder<'ctx>,
+    fn_value: inkwell::values::FunctionValue<'ctx>,
     expr: &Expr,
     params: &[(String, usize)],
-    location: Location<'c>,
-) -> Result<melior::ir::Value<'c, 'a>> {
+) -> Result<BasicValueEnum<'ctx>> {
     match expr {
         Expr::Lit(Lit::Num(num)) => {
-            let i64_type = IntegerType::new(context, 64).into();
-            let op = block.append_operation(arith::constant(
-                context,
-                IntegerAttribute::new(i64_type, num.value as i64).into(),
-                location,
-            ));
-            Ok(op.result(0).unwrap().into())
+            let i64_type = context.i64_type();
+            Ok(i64_type.const_int(num.value as u64, false).into())
         }
         Expr::Ident(ident) => {
             let name = ident.sym.as_str();
             if let Some((_, idx)) = params.iter().find(|(n, _)| n == name) {
-                Ok(block.argument(*idx).unwrap().into())
+                Ok(fn_value.get_nth_param(*idx as u32).unwrap())
             } else {
                 Err(anyhow::anyhow!("unknown identifier: {name}"))
             }
         }
         Expr::Bin(bin) => {
-            let lhs = compile_expr(context, block, &bin.left, params, location)?;
-            let rhs = compile_expr(context, block, &bin.right, params, location)?;
-            let op = match bin.op {
-                BinaryOp::Add => block.append_operation(arith::addi(lhs, rhs, location)),
-                BinaryOp::Sub => block.append_operation(arith::subi(lhs, rhs, location)),
-                BinaryOp::Mul => block.append_operation(arith::muli(lhs, rhs, location)),
+            let lhs = compile_expr(context, builder, fn_value, &bin.left, params)?
+                .into_int_value();
+            let rhs = compile_expr(context, builder, fn_value, &bin.right, params)?
+                .into_int_value();
+            let result = match bin.op {
+                BinaryOp::Add => builder.build_int_add(lhs, rhs, "add")?,
+                BinaryOp::Sub => builder.build_int_sub(lhs, rhs, "sub")?,
+                BinaryOp::Mul => builder.build_int_mul(lhs, rhs, "mul")?,
+                BinaryOp::Div => builder.build_int_signed_div(lhs, rhs, "div")?,
+                BinaryOp::Mod => builder.build_int_signed_rem(lhs, rhs, "rem")?,
+                BinaryOp::Lt => builder
+                    .build_int_compare(IntPredicate::SLT, lhs, rhs, "lt")?
+                    .into(),
+                BinaryOp::LtEq => builder
+                    .build_int_compare(IntPredicate::SLE, lhs, rhs, "le")?
+                    .into(),
+                BinaryOp::Gt => builder
+                    .build_int_compare(IntPredicate::SGT, lhs, rhs, "gt")?
+                    .into(),
+                BinaryOp::GtEq => builder
+                    .build_int_compare(IntPredicate::SGE, lhs, rhs, "ge")?
+                    .into(),
+                BinaryOp::EqEqEq => builder
+                    .build_int_compare(IntPredicate::EQ, lhs, rhs, "eq")?
+                    .into(),
+                BinaryOp::NotEqEq => builder
+                    .build_int_compare(IntPredicate::NE, lhs, rhs, "ne")?
+                    .into(),
                 _ => return Err(anyhow::anyhow!("unsupported binary operator: {:?}", bin.op)),
             };
-            Ok(op.result(0).unwrap().into())
+            Ok(result.into())
         }
         _ => Err(anyhow::anyhow!("unsupported expression: {:?}", expr)),
     }
