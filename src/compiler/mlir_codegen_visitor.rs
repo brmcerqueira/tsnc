b470e80bba9496df6de330d8bd441dc71823b465
Bruno <brmcerqueira@gmail.com>
2026-08-16 12:08:37 -0300

    Start

--- src/compiler/mlir_codegen_visitor.rs
+++ src/compiler/mlir_codegen_visitor.rs
@@ -1,12 +1,16 @@
 use anyhow::{Result, anyhow};
+use melior::Context;
 use melior::dialect::arith;
-use melior::ir::{Block, BlockLike, Location, Value};
+use melior::ir::{Block, BlockLike, Location, Operation, Type, Value};
 use swc_ecma_ast::{BinExpr, BinaryOp, Expr};
 use swc_ecma_visit::{Visit, VisitWith};
 
 struct MLIRCodegenVisitor<'c> {
-    block: &'c Block<'c>,
+    context: &'c Context,
     location: Location<'c>,
+    i64_type: Type<'c>,
+    i32_type: Type<'c>,
+    block: &'c Block<'c>,
     last_value: Result<Value<'c, 'c>>,
 }
 
@@ -17,6 +21,28 @@ impl<'c> MLIRCodegenVisitor<'c> {
             Ok(v) => Ok(*v),
             Err(e) => Err(anyhow!("{e}")),
         }
+    }
+
+    fn comparison_operation(
+        &mut self,
+        predicate: arith::CmpiPredicate,
+        lhs: Value<'c, 'c>,
+        rhs: Value<'c, 'c>,
+    ) -> Result<Operation<'c>> {
+        Ok(arith::extui(
+            self.block
+                .append_operation(arith::cmpi(
+                    self.context,
+                    predicate,
+                    lhs,
+                    rhs,
+                    self.location,
+                ))
+                .result(0)?
+                .into(),
+            self.i64_type,
+            self.location,
+        ))
     }
 }
 
@@ -33,7 +59,13 @@ impl<'c> Visit for MLIRCodegenVisitor<'c> {
                 BinaryOp::Mul => arith::muli(lhs, rhs, self.location),
                 BinaryOp::Div => arith::divsi(lhs, rhs, self.location),
                 BinaryOp::Mod => arith::remsi(lhs, rhs, self.location),
-                _ => return Err(anyhow!("unsupported binary operator: {:?}", expr.op))
+                BinaryOp::Lt => self.comparison_operation(arith::CmpiPredicate::Slt, lhs, rhs)?,
+                BinaryOp::LtEq => self.comparison_operation(arith::CmpiPredicate::Sle, lhs, rhs)?,
+                BinaryOp::Gt => self.comparison_operation(arith::CmpiPredicate::Sgt, lhs, rhs)?,
+                BinaryOp::GtEq => self.comparison_operation(arith::CmpiPredicate::Sge, lhs, rhs)?,
+                BinaryOp::EqEqEq => self.comparison_operation(arith::CmpiPredicate::Eq, lhs, rhs)?,
+                BinaryOp::NotEqEq => self.comparison_operation(arith::CmpiPredicate::Ne, lhs, rhs)?,
+                _ => return Err(anyhow!("unsupported binary operator: {:?}", expr.op)),
             };
 
             return Ok(self.block.append_operation(operation).result(0)?.into());
