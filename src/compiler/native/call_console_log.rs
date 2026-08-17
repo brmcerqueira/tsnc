682e993e304fbd84fc92b19de6ef6c387704c721
Bruno <brmcerqueira@gmail.com>
2026-08-17 18:42:55 -0300

    Start

--- src/compiler/native/call_console_log.rs
+++ src/compiler/native/call_console_log.rs
@@ -1,10 +1,14 @@
-use anyhow::anyhow;
+use anyhow::{Result, anyhow};
 use melior::ir::Value;
 use crate::compiler::mlir_codegen_visitor::MLIRCodegenVisitor;
 
 pub(super) fn call_console_log<'c>(
     visitor: &mut MLIRCodegenVisitor<'c>,
     args: &Vec<Value>,
-) -> anyhow::Result<Value<'c, 'c>> {
+) -> Result<Value<'c, 'c>> {
+    if args.len() != 1 {
+        return Err(anyhow!("console.log expects exactly one argument"));
+    }
+
     Err(anyhow!("can't resolve native call for"))
 }
