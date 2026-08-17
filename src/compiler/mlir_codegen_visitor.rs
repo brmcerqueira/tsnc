f8adf3539a4683a2418be9fa62d10f79b91be9b1
Bruno <brmcerqueira@gmail.com>
2026-08-17 01:41:24 -0300

    Start

--- src/compiler/mlir_codegen_visitor.rs
+++ src/compiler/mlir_codegen_visitor.rs
@@ -1,20 +1,22 @@
-use std::collections::HashMap;
 use super::visit_bin_expr::visit_bin_expr;
+use super::visit_call_expr::visit_call_expr;
 use super::visit_ident::visit_ident;
 use super::visit_lit::visit_number;
 use anyhow::{Result, anyhow};
 use melior::Context;
 use melior::ir::{Block, Value};
-use swc_ecma_ast::{BinExpr, CallExpr, Expr, Ident, Number};
+use std::collections::HashMap;
+use swc_ecma_ast::{BinExpr, CallExpr, Expr, FnDecl, Ident, Number};
 use swc_ecma_visit::{Visit, VisitWith};
-use super::visit_call_expr::visit_call_expr;
 
 pub(super) type Vars<'c> = HashMap<String, Value<'c, 'c>>;
+pub(super) type Functions<'c> = HashMap<String, &'c FnDecl>;
 
 pub(super) struct MLIRCodegenVisitor<'c> {
     pub(super) context: &'c Context,
     pub(super) block: &'c Block<'c>,
     pub(super) vars: Vars<'c>,
+    pub(super) functions: Functions<'c>,
     pub(super) last_value: Result<Value<'c, 'c>>,
 }
 
@@ -27,9 +29,14 @@ impl<'c> MLIRCodegenVisitor<'c> {
         }
     }
 }
+
 impl<'c> Visit for MLIRCodegenVisitor<'c> {
     fn visit_bin_expr(&mut self, node: &BinExpr) {
         self.last_value = visit_bin_expr(self, node);
+    }
+
+    fn visit_call_expr(&mut self, node: &CallExpr) {
+        self.last_value = visit_call_expr(self, node);
     }
 
     fn visit_ident(&mut self, node: &Ident) {
@@ -38,9 +45,5 @@ impl<'c> Visit for MLIRCodegenVisitor<'c> {
 
     fn visit_number(&mut self, node: &Number) {
         self.last_value = visit_number(self, node);
-    }
-
-    fn visit_call_expr(&mut self, node: &CallExpr) {
-        self.last_value = visit_call_expr(self, node);
     }
 }
--- src/compiler/visit_call_expr.rs
+++ src/compiler/visit_call_expr.rs
@@ -1,9 +1,10 @@
 use super::mlir_codegen_visitor::MLIRCodegenVisitor;
 use anyhow::{Result, anyhow};
-use melior::dialect::func;
-use melior::ir::attribute::FlatSymbolRefAttribute;
+use melior::dialect::{arith, func};
+use melior::ir::attribute::{FlatSymbolRefAttribute, IntegerAttribute};
+use melior::ir::r#type::IntegerType;
 use melior::ir::{BlockLike, Location, Value};
-use swc_ecma_ast::{CallExpr, Callee, Expr, ExprOrSpread, MemberProp};
+use swc_ecma_ast::{CallExpr, Callee, Expr, ExprOrSpread, MemberProp, TsKeywordTypeKind, TsType};
 
 pub(super) fn visit_call_expr<'c>(
     visitor: &mut MLIRCodegenVisitor<'c>,
@@ -29,14 +30,36 @@ pub(super) fn visit_call_expr<'c>(
                 Err(anyhow!("unsupported method call"))
             }
             Expr::Ident(ident) => {
-                let result_types = vec![];
-                Ok(visitor.block.append_operation(func::call(
-                    visitor.context,
-                    FlatSymbolRefAttribute::new(visitor.context, ident.sym.as_ref()),
-                    &args,
-                    &result_types,
-                    Location::unknown(visitor.context),
-                )).result(0)?.into())
+                let name = ident.sym.as_ref();
+                let result_types = visitor
+                    .functions
+                    .get(name)
+                    .copied()
+                    .ok_or_else(|| anyhow!("unknown function: {}", name))
+                    .map(|function| match &function.function.return_type {
+                        None => vec![],
+                        Some(ann) => match ann.type_ann.as_ref() {
+                            TsType::TsKeywordType(kw) => {
+                                if kw.kind == TsKeywordTypeKind::TsVoidKeyword {
+                                    vec![]
+                                } else {
+                                    vec![IntegerType::new(visitor.context, 64).into()]
+                                }
+                            }
+                            _ => vec![IntegerType::new(visitor.context, 64).into()],
+                        },
+                    })?;
+                Ok(visitor
+                    .block
+                    .append_operation(func::call(
+                        visitor.context,
+                        FlatSymbolRefAttribute::new(visitor.context, name),
+                        &args,
+                        &result_types,
+                        Location::unknown(visitor.context),
+                    ))
+                    .result(0)?
+                    .into())
             }
             _ => Err(anyhow!("unsupported callee expression")),
         },
